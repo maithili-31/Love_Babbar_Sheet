5de34cce8b90617f7e7f30d207cd76b720995ee0
maithili-31 <103103546+maithili-31@users.noreply.github.com>
2022-11-20 18:20:05 +0530

    Create Reverse the Array.cpp

---
+++ Reverse the Array.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+// this can be also used
+// void swap(int *a, int *b){
+//     int temp = *a;
+//     *a = *b ;
+//     *b = temp;
+// };
+
+int main(){
+	//code
+	int T , temp , n ;
+	cin>>T;
+	for(int i= 0 ; i<T ; i++){
+	    cin>>n;
+	    int arr[n];
+	    for(int i = 0 ; i<n ; i++){
+	        cin>>arr[i]; 
+	    }
+	    for(int i = 0 ; i<n/2 ; i++){
+	       // swap(arr[i],arr[n-i-1]);
+	        temp = arr[i];
+            arr[i] = arr[n-i-1] ;
+            arr[n-i-1] = temp;
+	    }
+	    for(int i = 0 ; i<n ; i++){
+	        cout<<arr[i]<<" "; 
+	    }
+	    cout<<endl;
+	}
+	return 0;
+}
