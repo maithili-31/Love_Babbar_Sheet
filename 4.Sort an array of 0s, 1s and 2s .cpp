473681a2127278ab32dcd9820479538ee505e22c
Maithili Chhallani <103103546+maithili-31@users.noreply.github.com>
2022-11-25 23:49:36 +0530

    basic

--- 4.Sort an array of 0s, 1s and 2s .cpp
+++ 4.Sort an array of 0s, 1s and 2s .cpp
@@ -25,4 +25,18 @@ void sort012(int a[], int n)
         for(int i = 0 ; i<n ; i++){
             a[i] = b[i];
         }
+//     optimised approach
+//     int l = 0, r = n - 1, m = 0;
+//         while (m <= r) {
+//             if (a[m] == 0) {
+//                 swap(a[l], a[m]);
+//                 l++;
+//             }
+//             if (a[m] == 2) {
+//                 swap(a[m], a[r]);
+//                 r--;
+//                 continue;
+//             }
+//             m++;
+//         }
     }
