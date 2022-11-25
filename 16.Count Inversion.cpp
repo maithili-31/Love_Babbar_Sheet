83e7cb99fc86531aa682407a206b234866c5156f
Maithili Chhallani <103103546+maithili-31@users.noreply.github.com>
2022-11-26 00:42:29 +0530

    incomplete

--- 16.Count Inversion.cpp
+++ 16.Count Inversion.cpp
@@ -1,36 +1,32 @@
 long long int inversionCount(long long arr[], long long N)
     {
         // Your Code Here
-//         TLE:
         int i , j , a ;
         i = a = 0 ;
         j = i+1;
         int count = 0 ;
-        // int b[N];
-        // for(int i = 0 ; i < N ; i++){
-        //     b[i] = arr[i];
-        // }
-        // sort(b,b+N);
         
-        
+//     TLE:
         while(j<N and i<N){
-            if(arr[i] > arr[j] and (j != N-1)){
+            if(arr[i] > arr[j]){
                 count++;
-                j++;
-            }
-            else if(arr[i] < arr[j] and  (j != N-1)){
-                j++;
-            }
-            
-            else if(arr[i] > arr[j] and (j == N-1)){
-                count++;
-                i++;
-                j = i+1;
-            }
-            
-            else if(arr[i] < arr[j] and (j == N-1)){
-                i++;
-                j = i+1;
+                if(j != N-1){
+                    j++;
+                }
+                else{
+                    i++;
+                    j = i+1;
+                }
+                
+            } 
+            else if(arr[i] < arr[j]){
+                if(j != N-1){
+                    j++;
+                }
+                else{
+                    i++;
+                    j = i+1;
+                }
             }
         }
         return count;
